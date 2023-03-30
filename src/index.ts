b932274a5c2f658c767a740329456b4825bf7440
jonas-45 <jonaswullah@gmail.com>
2023-03-30 10:35:36 +0000

    Add functions and event listeners to save, retrieve and display todo list items

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,63 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidv4 } from 'uuid';
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
 
-import confetti from 'canvas-confetti';
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.getElementById('list-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const tasks: Task[] = loadTasks();
+tasks.forEach((task) => {
+  addListItem(task);
+})
+
+form?.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if(input?.value == '' || input?.value == null) return;
+
+  const newTask = {
+    id: uuidv4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  
+  tasks.push(newTask);
+  addListItem(newTask);
+  saveTasks();
+
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  })
+
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+  form?.reset();
+}
+
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const tasksJson = localStorage.getItem('tasks');
+  if(tasksJson === null) return [];
+  return JSON.parse(tasksJson);
+}
